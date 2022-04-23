9f579fd58d5d708cd67839207283fa7483d86df7
nvthanhtu <nguyenvanthanhtu@gmail.com>
2022-04-23 13:08:49 +0200

    add level + modif image folder

---
+++ captcha_reader.py
@@ -0,0 +1,16 @@
+import numpy as np
+from PIL import ImageGrab
+
+
+def is_pin_code_required(screen):
+    
+    cv2.imshow('image window', image)
+    
+    return False
+    
+def main():
+    printscreen =  np.array(ImageGrab.grab(bbox=(600,100,1400,1200)))
+        
+    
+if __name__ == "__main__":
+    main()
